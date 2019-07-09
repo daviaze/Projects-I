ba7efa4cf3212cf8c7ed5376655157916f37064a
Davi Azevedo <47645089+daviaze@users.noreply.github.com>
2019-07-09 17:04:05 -0300

    SliceFrutasEx1

---
+++ slice1.js
@@ -0,0 +1,2 @@
+let frutas = ["Abacaxi", "Maça", "Melancia", "Banana", "Uva"]
+console.log(frutas.slice(1, 3))//retorna um subarray com parametros de inicio e fim, obs:(o fim não é inserido no array)
